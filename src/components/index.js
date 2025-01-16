d35b2a64ae2e0a100be6fd589b6640c975fc4ca4
lightningbat <183808230+lightningbat@users.noreply.github.com>
2025-01-16 19:26:47 +0530

    Moved NavBar to separate file

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,14 @@ import SearchBox from "./search_box";
 import AuthenticationWindow from "./authentication_window";
 import CollegeCard from "./college_card";
 import ProfessorCard from "./professor_card";
+import OutsideClickDetector from "./outside_click_detector";
+import NavBar from "./nav_bar";
 
 export {
     SearchBox,
     AuthenticationWindow,
     CollegeCard,
-    ProfessorCard
+    ProfessorCard,
+    OutsideClickDetector,
+    NavBar
 }
