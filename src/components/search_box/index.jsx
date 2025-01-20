452abe3106fd22988529c3f73f0539a0f1a030d3
lightningbat <183808230+lightningbat@users.noreply.github.com>
2025-01-20 21:06:25 +0530

    Implemented search functionality of CollegesListPage by connecting SearchBox with backend

--- src/components/search_box/index.jsx
+++ src/components/search_box/index.jsx
@@ -1,13 +1,21 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable react/display-name */
 import './style.scss'
 import { forwardRef } from 'react'
 
+/**
+ * @param {Object} props - contains {placeholder, pattern, disabled}
+ * @param {string} props.placeholder - placeholder of the input
+ * @param {string} props.pattern - pattern for the input
+ * @param {boolean} props.disabled - disabled state of the input
+ */
 const SearchBox = forwardRef((props, ref) => {
     return (
         <div className="search-box">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-search" viewBox="0 0 16 16">
                 <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
             </svg>
-            <input ref={ref} type="text" placeholder='Search' />
+            <input ref={ref} type="text" placeholder={props.placeholder || "Search"} pattern={props.pattern} disabled={props.disabled} />
         </div>
     )
 })
--- src/pages/colleges_list/index.jsx
+++ src/pages/colleges_list/index.jsx
@@ -1,6 +1,6 @@
 import './style.scss'
 import { SearchBox, CollegeCard } from '../../components'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import useCustomDialog from '../../custom/dialogs';
 
 let cached_data = [];
@@ -8,9 +8,24 @@ let cached_data = [];
 export default function CollegesListPage() {
     const customDialogs = useCustomDialog();
     const [collegesList, setCollegesList] = useState([]);
+    const search_box_ref = useRef();
+    const [searchMsg, setSearchMsg] = useState("");
 
+    async function fetchWrapper(url, options) {
+        try {
+            const response = await fetch(url, options);
+            return response;
+        }
+        catch (error) {
+            console.error(error);
+            return null;
+        }
+    }
+
+    // startup functions
     useEffect(() => {
-        console.log(cached_data);
+
+        // getting colleges list
         (async () => {
             if (cached_data.length && !collegesList.length) {
                 setCollegesList(cached_data);
@@ -24,9 +39,7 @@ export default function CollegesListPage() {
 
                 if (response.ok) {
                     const data = await response.json();
-                    console.log(data);
                     setCollegesList(data);
-                    // populating data
                     cached_data = data;
                 } else {
                     customDialogs({
@@ -49,12 +62,81 @@ export default function CollegesListPage() {
                 console.error(error);
             }
         })()
+
+        /********* search box functions ***********/
+        const handleInputChange = (event) => {
+            // removing all non-alphabets and non-spaces
+            event.target.value = event.target.value.replace(/[^a-zA-Z ]/g, '');
+            // trimming spaces
+            const user_input = (event.target.value).trim();
+            
+            if (user_input == "") setSearchMsg("");
+
+            const search_result = cached_data.filter((item) => item.name.toLowerCase().includes(user_input.toLowerCase()));
+            if (search_result.length && user_input != "") {
+                setSearchMsg("Hit Enter to look in the Database");
+            }
+            setCollegesList(search_result);
+        }
+
+        const handleInputSearch = async (event) => {
+            if (event.key === "Enter") {
+                event.target.blur(); // removing focus from search box
+                const user_input = event.target.value.trim();
+                event.target.disabled = true; // disabling search box to prevent spamming
+                setSearchMsg("Searching...");
+                setCollegesList([]); // clearing colleges list for a fell of item being searched
+
+                const response = await fetchWrapper(`${import.meta.env.VITE_SERVER_URL}/find-college`, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify({ user_query: user_input })
+                })
+
+                event.target.disabled = false; // enabling search box
+
+                if (response.ok) {
+                    const data = await response.json();
+                    setCollegesList(data);
+                    setSearchMsg(`Found ${data.length} results for "${user_input}"`);
+
+                    // adding new data to cached data (if not present)
+                    if (data.length) {
+                        data.forEach((item) => {
+                            if (!cached_data.find((cached_item) => cached_item.id === item.id)) {
+                                cached_data.push(item);
+                            }
+                        })
+                    }
+                } else {
+                    customDialogs({
+                        type: "alert",
+                        title: "Error",
+                        description: "Failed to get colleges list"
+                    })
+                }
+            }
+        }
+
+        const search_box = search_box_ref.current;
+        search_box.addEventListener("input", handleInputChange);
+        search_box.addEventListener("keydown", handleInputSearch);
+
+        return () => {
+            search_box.removeEventListener("input", handleInputChange);
+            search_box.removeEventListener("keydown", handleInputSearch);
+        }
     }, [])
     
     return (
         <div className="colleges-page">
             <h3 className='page-heading'>Look for your college</h3>
-            <div className='search-box-cont'><SearchBox /></div>
+            <div className='search-box-cont'>
+                <SearchBox placeholder="Search Colleges" pattern="^[a-zA-Z ]+$" ref={search_box_ref} />
+                <div className="search-msg">{searchMsg}</div>
+            </div>
             <div className="list-container">
                 {
                     collegesList.map((item) => (
