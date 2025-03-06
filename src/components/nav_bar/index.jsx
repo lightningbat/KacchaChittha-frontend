f9498a26ccc826e013c8f5d117a2a07809ed6e1d
lightningbat <183808230+lightningbat@users.noreply.github.com>
2025-03-06 06:21:30 +0530

    Added visual field for username

--- src/components/nav_bar/index.jsx
+++ src/components/nav_bar/index.jsx
@@ -134,7 +134,7 @@ export default function NavBar({ setShowAuthWindow, setShowProfessorForm }) {
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="menu-item-icon" viewBox="0 0 16 16">
                                             <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z" />
                                         </svg>
-                                        <p className="menu-item-text">{userDetails.name}</p>
+                                        <p className="menu-item-text">{userDetails.name} | <span className="username">{userDetails.username}</span></p>
                                     </div>
                                     <div className="menu-item">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="menu-item-icon email-icon" viewBox="0 0 16 16">
