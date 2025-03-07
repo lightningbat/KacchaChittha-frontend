76567d3a4b9d798e02e963c3c3ea446d6fc1c4e1
lightningbat <183808230+lightningbat@users.noreply.github.com>
2025-03-07 21:50:04 +0530

    Changed contact-us email id

--- src/components/nav_bar/index.jsx
+++ src/components/nav_bar/index.jsx
@@ -156,7 +156,7 @@ export default function NavBar({ setShowAuthWindow, setShowProfessorForm }) {
                                         </svg>
                                         <p className="menu-item-text">Logout</p>
                                     </div>
-                                    <a className="menu-item contact-us" href="mailto:spnae.severus@gmail.com?subject=Feedback and Suggestions for RateMyProfessor" target="_blank" rel="noopener noreferrer">
+                                    <a className="menu-item contact-us" href="mailto:kacchachitthaa@gmail.com?subject=Feedback and Suggestions for RateMyProfessor" target="_blank" rel="noopener noreferrer">
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="menu-item-icon email-icon" viewBox="0 0 16 16">
                                             <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2zm13 2.383l-4.758 2.855L15 11.114v-5.73zm-.034 6.878L9.271 8.82 8 9.583 6.728 8.82l-5.694 3.44A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.739zM1 11.114l4.758-2.876L1 5.383v5.73z" />
                                         </svg>
